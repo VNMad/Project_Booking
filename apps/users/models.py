6f37d198029e1145ab5ce934b18a2889d847aa5c
VNMad <vadiknoname@gmail.com>
2026-09-10 09:20:49 +0200

    models Booking_User

--- apps/users/models.py
+++ apps/users/models.py
@@ -22,7 +22,7 @@ class BookingUserManager(UserManager):
 
         return self.create_user(email=email, password=password, **extra_fields)
 
-class Booking_User(UniqueID, TimeStampedModel, AbstractUser):
+class Booking_User(TimeStampedModel, AbstractUser):
     username = None
 
     first_name = models.CharField(max_length=25, verbose_name=_("First name"))
